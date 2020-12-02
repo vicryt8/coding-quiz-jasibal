996110e5268ab97a70bc8e4e2427dcb2ca2217e7
josaye2 <j.m.asibal@hotmail.com>
2020-12-03 03:46:52 +1100

    randomised question/answer order

--- script.ts
+++ script.ts
@@ -3,47 +3,67 @@ var questions = [
 		title: "Commonly used data types DO NOT include:",
 		choices: ["strings", "booleans", "alerts", "numbers"],
 		answer: "alerts",
+		respectOrder: false,
+	},
+	{
+		title: "Commonly used data types DO NOT include:",
+		choices: ["strings", "booleans", "alerts", "numbers"],
+		answer: "alerts",
+		respectOrder: false,
 	},
 	{
 		title: "The condition in an if / else statement is enclosed within ____.",
 		choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
 		answer: "parentheses",
+		respectOrder: false,
 	},
 	{
 		title: "Arrays in JavaScript can be used to store ____.",
 		choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
 		answer: "all of the above",
+		respectOrder: true,
 	},
 	{
 		title: "String values must be enclosed within ____ when being assigned to variables.",
 		choices: ["commas", "curly brackets", "quotes", "parentheses"],
 		answer: "quotes",
+		respectOrder: false,
 	},
 	{
 		title: "A very useful tool used during development and debugging for printing content to the debugger is:",
 		choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
 		answer: "console.log",
+		respectOrder: false,
 	},
-	{ title: "Commonly used data types DO NOT include:", choices: ["strings", "booleans", "alerts", "numbers"], answer: "alerts" },
 	{
-		title: "The condition in an if / else statement is enclosed within ____.",
+		title: "YEET",
+		choices: ["strings", "booleans", "alerts", "numbers"],
+		answer: "alerts",
+		respectOrder: false,
+	},
+	{
+		title: "Tquestion ",
 		choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
 		answer: "parentheses",
+		respectOrder: false,
 	},
 	{
-		title: "Arrays in JavaScript can be used to store ____.",
+		title: "Question 8",
 		choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
 		answer: "all of the above",
+		respectOrder: true,
 	},
 	{
-		title: "String values must be enclosed within ____ when being assigned to variables.",
+		title: "Questino 9",
 		choices: ["commas", "curly brackets", "quotes", "parentheses"],
 		answer: "quotes",
+		respectOrder: false,
 	},
 	{
-		title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+		title: "Question 10",
 		choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
 		answer: "console.log",
+		respectOrder: false,
 	},
 ];
 const maxTime = 75;
@@ -69,13 +89,15 @@ var currentTime = maxTime + 1;
 var quizComplete = false;
 var quizTimer: any;
 var textFade: any;
+var questionOrder: number[];
 
 function initQuizState(fromContainer?: HTMLElement): void {
 	console.log(fromContainer.id);
+	questionOrder = genNumberArray(0, questions.length - 1, false);
 	newQuizTimer();
 	quizTimer = setInterval(newQuizTimer, 1000);
 	displayElements(fromContainer, quizContainer);
-	displayQuestion(q);
+	displayQuestion();
 }
 function newQuizTimer(): void {
 	console.log("iq: " + currentTime);
@@ -85,18 +107,19 @@ function newQuizTimer(): void {
 		timerElement.textContent = "Time Remaining: " + String(currentTime);
 	}
 }
-function validateTime() {
+function validateTime(): void {
 	console.log("vt");
 	if (currentTime <= 0 && quizComplete === false) {
 		endQuiz(0);
 	}
 }
-function displayQuestion(currentQuestionIndex: number): void {
+function displayQuestion(): void {
 	console.log("dq");
-	quizNumber.textContent = "Question " + String(currentQuestionIndex + 1) + "/" + String(questions.length);
-	quizHeader.textContent = questions[currentQuestionIndex].title;
+	quizNumber.textContent = "Question " + String(q + 1) + "/" + String(questions.length);
+	quizHeader.textContent = questions[questionOrder[q]].title;
+	var answerOrder = genNumberArray(0, questions[questionOrder[q]].choices.length - 1, questions[questionOrder[q]].respectOrder);
 	for (var i = 0; i < 4; i++) {
-		document.querySelector("#" + "option-" + String(i + 1)).innerHTML = String(i + 1) + ". " + questions[currentQuestionIndex].choices[i];
+		document.querySelector("#" + "option-" + String(i + 1)).innerHTML = String(i + 1) + ". " + questions[questionOrder[q]].choices[answerOrder[i]];
 	}
 }
 function validateAnwer(event): void {
@@ -104,12 +127,12 @@ function validateAnwer(event): void {
 	clearTimeout(textFade);
 	console.log("validateAnswer " + q);
 	if (q < questions.length - 1) {
-		var incorrectPenalty = event.target.innerText.search(questions[q].answer) > 0 ? 0 : penalty;
+		var incorrectPenalty = event.target.innerText.search(questions[questionOrder[q]].answer) > 0 ? 0 : penalty;
 		currentTime = currentTime - incorrectPenalty > 0 ? currentTime - incorrectPenalty : 0;
 		timerElement.textContent = "Time Remaining: " + String(currentTime);
 		var isCorrect = incorrectPenalty === 0 ? "Correct 😀" : "Wrong 😣";
 		q++;
-		displayQuestion(q);
+		displayQuestion();
 		displayResult(isCorrect);
 	} else {
 		quizComplete = true;
@@ -141,6 +164,19 @@ function endQuiz(score: number) {
 function displayElements(hideElement: HTMLElement, showElement: HTMLElement): void {
 	hideElement.style.display = "none";
 	showElement.style.display = "block";
+}
+function genNumberArray(start: number, end: number, respectOrder: boolean): number[] {
+	var sequentialArray = Array(end - start + 1)
+		.fill(0)
+		.map((item, index) => start + index);
+	var randomisedArray = [...sequentialArray];
+	for (let i = randomisedArray.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[randomisedArray[i], randomisedArray[j]] = [randomisedArray[j], randomisedArray[i]];
+	}
+	console.log("random: " + randomisedArray);
+	console.log("seq: " + sequentialArray);
+	return respectOrder ? sequentialArray : randomisedArray;
 }
 
 anwerBtns.forEach((item) => {
